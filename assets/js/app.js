79f0a115e4a90d724945e17635a5e98421dd8045
NatyNatur <naticcg@gmail.com>
2018-01-10 23:04:04 -0300

    arreglando detalles en index y app

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,7 +19,7 @@
  $('#myModal').modal();
 
  function getMovies(searchText){
-  axios.get('http://www.omdbapi.com/?s=' + searchText + '&apikey=fcd50d7e')
+  axios.get('https://www.omdbapi.com/?s=' + searchText + '&apikey=fcd50d7e')
   .then((response) => {
     console.log(response);
     let movies = response.data.Search;
@@ -51,7 +51,7 @@ function movieSelected(id){
 
 function getMovie(){
   let movieId = sessionStorage.getItem('movieId');
-  axios.get('http://www.omdbapi.com/?i=' + movieId + '&apikey=fcd50d7e')
+  axios.get('https://www.omdbapi.com/?i=' + movieId + '&apikey=fcd50d7e')
   .then((response) => {
     console.log(response);
     let movie = response.data;
