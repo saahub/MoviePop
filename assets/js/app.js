9c4c39718586236bd75548227f6f4e888241e69a
kuveee <kamilavillablanca23@gmail.com>
2018-01-11 10:14:50 -0300

    actualizando espacios en carrusel

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,29 +12,12 @@
     $('#contact').hide();
     $('#mapa').hide();
     $('#releases').hide();
+    $('.swiper').hide();
     $('#searchText').val('');
-    
-    // carousel
-var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        effect: 'coverflow',
-        grabCursor: true,
-        centeredSlides: true,
-  spaceBetween: 0,
-        //loop: true,
-autoplay: 2500,
-        autoplayDisableOnInteraction: false,
-        slidesPerView: 4,
-        coverflow: {
-            rotate: 30,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows : true
-        };
-    });
+        }
+    );
   });
-});
+
 
  $('#myModal').modal();
 
